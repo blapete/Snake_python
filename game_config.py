4b0a4b724e94fb0de7f5ff75e439a833243b0e8b
blapete <pjblank2@gmail.com>
2020-11-09 17:40:34 -0500

    rgb color configuration for the game

---
+++ game_config.py
@@ -0,0 +1,9 @@
+class GameConfig():
+    # game colors
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    RED = (255,0,0)
+    GREEN = (0,255,0)
+    DARKGREEN = (0,155,0)
+    DARKGRAY = (40,40,40)
+    BG_COLOR = BLACK
