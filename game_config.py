c822a47e2061eac77e371d630871ed196f58c793
blapete <pjblank2@gmail.com>
2020-11-09 20:14:32 -0500

    game config file

--- game_config.py
+++ game_config.py
@@ -1,4 +1,19 @@
 class GameConfig():
+    FPS = 8
+    WINDOW_WIDTH = 640
+    WINDOW_HEIGHT = 480
+    CELLSIZE = 20
+    assert WINDOW_WIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+    assert WINDOW_HEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+    CELLWIDTH = int(WINDOW_WIDTH / CELLSIZE)
+    CELLHEIGHT = int(WINDOW_HEIGHT / CELLSIZE)
+
+    
+    # window dimension = 640 x 480
+    # if cell size = 20 ...
+    # cell count = 32 x 24
+
+
     # game colors
     BLACK = (0,0,0)
     WHITE = (255,255,255)
